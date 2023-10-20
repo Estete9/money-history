87c100bdd0708bd34b2c8d494cf40d8633a44fb2
Esteban <cepalacios@outlook.com>
2023-10-20 10:23:57 -0500

    Add test for headerSlice and currenciesSlice

---
+++ src/tests/currenciesSlice.test.js
@@ -0,0 +1,7 @@
+import currenciesReducer, { fetchSymbolsAPI } from '../redux/currencies/currenciesSlice';
+
+test('currenciesSlice is loading', () => {
+  let store = null;
+  store = currenciesReducer(undefined, fetchSymbolsAPI.pending());
+  expect(store.isLoadingRockets).toBe(true);
+});
