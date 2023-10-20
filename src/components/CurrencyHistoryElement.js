4db07bff7cc4c7e3a37e0009d9b295f4973168ef
Esteban <cepalacios@outlook.com>
2023-10-20 09:49:28 -0500

    Add  styles to currenciesHistory and currenciesHistoryElement

--- src/components/CurrencyHistoryElement.js
+++ src/components/CurrencyHistoryElement.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import styles from '../styles/currencyHistoryElement.module.css';
+import arrowRightImg from '../assets/arrow-right.svg';
 
 function CurrencyHistoryElement({ currency }) {
   return (
-    <div>
+    <div className={styles.currencyHistoryElementWrapper}>
       <div>{currency.year}</div>
-      <div>{`${currency.currencySymbol} ${currency.value} = EUR 1`}</div>
+      <p className={styles.currencyConversion}>{`${currency.currencySymbol} ${currency.value} = EUR 1`}</p>
+      <img className={styles.historyArrowImg} src={arrowRightImg} alt="enter currency history button" />
     </div>
   );
 }
