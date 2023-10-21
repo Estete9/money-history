446d6b92da277151e78da40900032366821d3a16
Esteban <cepalacios@outlook.com>
2023-10-21 09:16:06 -0500

    Implement describe with 2 tests, check if its showing the loaded data correctly

--- src/tests/currenciesList.test.js
+++ src/tests/currenciesList.test.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
+import { waitFor } from '@testing-library/react';
+import axios from 'axios';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import CurrenciesList from '../components/CurrenciesList';
 
+jest.mock('axios');
 const mockStore = configureStore([]);
 
 describe('Integration tests for CurrenciesList component', () => {
+  test('Checks if CurrencyList renders correctly after async API consumption', async () => {
+    const mockData = [
+      {
+        currencyCountry: 'Australian Dollar',
+        currencySymbol: 'AUD',
+      },
+      {
+        currencyCountry: 'Canadian Dollar',
+        currencySymbol: 'CAD',
+      },
+    ];
+    axios.get.mockResolvedValue({ data: mockData });
+    const currenciesStore = mockStore({
+      currencies: {
+        currenciesData: mockData,
+        isLoadingData: false,
+      },
+      header: {
+        searchQuery: '',
+        isSearchBarOpen: '',
+      },
+    });
+
+    const component = renderer.create(
+      <Provider store={currenciesStore}>
+        <MemoryRouter>
+          <CurrenciesList />
+        </MemoryRouter>
+      </Provider>,
+    );
+    const currenciesListTree = component.toJSON();
+
+    expect(currenciesListTree).toMatchSnapshot();
+  });
   test('Checks if loading screen in CurrencyList renders correctly', () => {
     const currenciesStore = mockStore({
       currencies: {
@@ -20,11 +58,14 @@ describe('Integration tests for CurrenciesList component', () => {
     });
     const component = renderer.create(
       <Provider store={currenciesStore}>
-        <CurrenciesList />
+        <MemoryRouter>
+          <CurrenciesList />
+        </MemoryRouter>
       </Provider>,
     );
     const currenciesListTree = component.toJSON();
 
     expect(currenciesListTree).toMatchSnapshot();
   });
+
 });
