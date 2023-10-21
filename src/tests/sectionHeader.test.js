7195e470d851a4f8eb35c4634fdc91f8fb05d6f4
Esteban <cepalacios@outlook.com>
2023-10-20 21:24:14 -0500

    Implement sectionHeader test

---
+++ src/tests/sectionHeader.test.js
@@ -0,0 +1,21 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import SectionHeader from '../components/SectionHeader';
+
+test('section header renders correctly', () => {
+  const mockValues = {
+    countryName: 'United States Dollar',
+    value: '1.0558',
+    symbol: 'USD',
+  };
+  const component = renderer.create(
+    <SectionHeader
+      symbol={mockValues.symbol}
+      value={mockValues.value}
+      countryName={mockValues.countryName}
+    />,
+  );
+  const sectionHeaderTree = component.toJSON();
+
+  expect(sectionHeaderTree).toMatchSnapshot();
+});
